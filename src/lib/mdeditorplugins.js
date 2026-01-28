1650bbea5bf7a4f03930162531b27e2e36e73e79
cenergy <673598118@qq.com>
2026-01-28 18:15:17 +0800

    refactor: modularize public directory and editor logic

--- src/lib/mdeditorplugins.js
+++ src/lib/mdeditorplugins.js
@@ -1,1 +1,16 @@
-"use strict";function registerMdPlugins(){var e=window.mdpress,r=window.prettier,i=window.prettierPlugins;r.prettierPlugins=i,e.registerPrettier(r),e.registerSwiper(window.Swiper),e.registerQRCode(window.QRCode),e.registerMermaid(window.mermaid),e.registerXLSX(window.XLSX),e.registerX_spreadsheet(window.x_spreadsheet),e.registerFlowChart(window.flowchart)}
+"use strict";
+function registerMdPlugins(){
+    var e=window.mdpress,r=window.prettier,i=window.prettierPlugins;
+    if(r && i) {
+        r.prettierPlugins=i;
+        e.registerPrettier(r);
+    }
+    if(window.Swiper) e.registerSwiper(window.Swiper);
+    if(window.QRCode) e.registerQRCode(window.QRCode);
+    if(window.mermaid) e.registerMermaid(window.mermaid);
+    if(window.XLSX) e.registerXLSX(window.XLSX);
+    if(window.x_spreadsheet) e.registerX_spreadsheet(window.x_spreadsheet);
+    if(window.flowchart) e.registerFlowChart(window.flowchart);
+}
+window.registerMdPlugins = registerMdPlugins;
+export default registerMdPlugins;
---
+++ src/utils/editor.js
@@ -0,0 +1,120 @@
+import * as monaco from 'monaco-editor'
+
+let mdEditor = null;
+
+export function getEditor() {
+    return mdEditor;
+}
+
+export function loadMonaco(callback) {
+    if (window.registerMdPlugins) {
+        window.registerMdPlugins();
+    }
+    if (window.mdpress) {
+        window.mdpress.registerMonaco(monaco);
+    }
+    if (callback) callback();
+}
+
+export function createEditor(selector, vm, callback) {
+    if (getEditor()) {
+        if (callback) callback();
+        return getEditor();
+    }
+
+    const mdpress = window.mdpress;
+    if (!mdpress) {
+        console.error("mdpress not found");
+        return;
+    }
+
+    const theme = vm.hero && vm.hero.theme ? vm.hero.theme : "vitepress";
+    
+    mdEditor = new mdpress.MDEditor(selector, {
+        autoParseVSCodePasteData: true,
+        // themeURL: "./theme/", 
+        monacoOptions: {
+            minimap: { enabled: false }
+        }
+    });
+    
+    mdEditor.setTheme(theme);
+
+    // Event listeners
+    const LEFT_NAV_FLOAT = "left-nav-float";
+    const ANIMATION_FADEINLEFT = "animate__fadeInLeft";
+
+    getEditor().on("closefullscreen", function() {
+        if (vm.$refs.leftnav) {
+            const classList = vm.$refs.leftnav.classList;
+            classList.remove(LEFT_NAV_FLOAT);
+            classList.remove(ANIMATION_FADEINLEFT);
+        }
+    });
+
+    // Paste handler
+    getEditor().on("paste", function(e) {
+        const files = e.clipboardData.files || [];
+        if (files.length > 0) {
+             // Basic implementation of file upload on paste
+             // The original logic filtered by size (20MB) and count (10)
+             // and called vm.uploadFile
+             Array.from(files).forEach(file => {
+                 if (file.size > 20 * 1024 * 1024) {
+                     vm.warn(`文件 ${file.name} 超过 20M，跳过上传`);
+                     return;
+                 }
+                 vm.uploadFile(file, (url) => {
+                     // Insert markdown image or link
+                     const isImage = file.type.startsWith('image/');
+                     const text = isImage ? `![${file.name}](${url})` : `[${file.name}](${url})`;
+                     
+                     const range = getEditor().getCurrentRange()[0];
+                     getEditor().editor.executeEdits("", [{ range: range, text: "\n" + text + "\n" }]);
+                 });
+             });
+        }
+    });
+
+    addToolicons(vm);
+
+    if (callback) callback();
+    return mdEditor;
+}
+
+function addToolicons(vm) {
+    const mdpress = window.mdpress;
+    const className = "majoricon";
+    const icons = [
+        { icon: "icon-zhankaicaidan", title: "打开左侧侧边栏", className: className, position: "right" },
+        { icon: "icon-file-markdown1", title: "导入markdown", className: className },
+        { icon: "icon-fujian1", title: "托管附件", className: className, position: "right" },
+        { icon: "icon-baocun1", title: "保存文档", className: className, position: "right" }
+    ].map(opts => new mdpress.ToolIcon(opts));
+
+    icons.forEach(icon => icon.addTo(getEditor()));
+
+    const LEFT_NAV_FLOAT = "left-nav-float";
+    const ANIMATION_FADEINLEFT = "animate__fadeInLeft";
+
+    icons[0].on("click", function() {
+        if (getEditor().isFullScreen()) {
+            if (vm.$refs.leftnav) {
+                const classList = vm.$refs.leftnav.classList;
+                if (classList.contains(LEFT_NAV_FLOAT)) {
+                    classList.remove(LEFT_NAV_FLOAT);
+                    classList.remove(ANIMATION_FADEINLEFT);
+                } else {
+                    classList.add(LEFT_NAV_FLOAT);
+                    classList.add(ANIMATION_FADEINLEFT);
+                }
+            }
+        } else {
+            vm.info("当编辑器全屏时才可以进行该操作");
+        }
+    });
+
+    icons[1].on("click", () => vm.importMd());
+    icons[2].on("click", () => vm.openUploadPanel());
+    icons[3].on("click", () => vm.saveDoc());
+}
