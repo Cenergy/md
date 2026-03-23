6c17e5b2cd3a99b5fe21dc414ffa2d62aa2913e9
cenergy <673598118@qq.com>
2026-03-23 16:40:28 +0800

    feat: 添加第三方库映射配置

---
+++ server/services/buildProject/thirdParty.js
@@ -0,0 +1,9 @@
+const thirdParty = {
+    "kate": "https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/KaTeX/0.5.1/kate",
+    "viewerjs": "https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/viewerjs/1.11.5/viewer.min.js",
+    "swiper": "https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/xlsx/0.18.5/xlsx.full.min.js",
+    "raphael": "https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/raphael/2.3.0/raphael.min.js",
+    "flowchart": "https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/flowchart/1.17.1/flowchart.min.js",
+}
+
+//在文章页head引入
